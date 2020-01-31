e7188dddc8b7a465cbad6d9224be7a7f2c0557e0
gracelove <govlmo91@gmail.com>
2020-01-31 13:39:10 +0900

    swagger 적용

--- src/main/java/me/gracelove/restapidemo/controller/v1/MemberController.java
+++ src/main/java/me/gracelove/restapidemo/controller/v1/MemberController.java
@@ -1,5 +1,7 @@
 package me.gracelove.restapidemo.controller.v1;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import me.gracelove.restapidemo.entity.Member;
 import me.gracelove.restapidemo.service.MemberService;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"1. Member"})
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/v1")
@@ -14,11 +17,13 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @ApiOperation(value = "회원조회", notes = "모든 회원 조회.")
     @GetMapping("/members")
     public List<Member> findAllMembers() {
         return memberService.findAll();
     }
 
+    @ApiOperation(value = "회원가입", notes = "회원가입")
     @PostMapping("/members")
     public Member save(@RequestBody Member member) {
         return memberService.create(member);
